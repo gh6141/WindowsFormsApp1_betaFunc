7be094b8563d283f8df27d30682d901e7dc4458c
Hidemitsu Goto <gh6141@gmail.com>
2024-12-04 15:57:24 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsFormsApp1_betaFunc/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1_betaFunc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "表示";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.comboBox1.Location = new System.Drawing.Point(709, 116);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(53, 20);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(688, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "r=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BetaRunction";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1_betaFunc/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+using OxyPlot.Series;
+using OxyPlot;
+using OxyPlot.WindowsForms; // Windowsフォームアプリケーション用
+
+namespace WindowsFormsApp1_betaFunc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PlotView plotView = new PlotView { Dock = System.Windows.Forms.DockStyle.Fill };
+            this.Controls.Add(plotView);
+
+            var model = new PlotModel { Title = "y = IntegrateA(x)" };
+            var series = new LineSeries { Title = "y = ∫[0, x] func dt" };
+
+            // 0～1の間で計算し、グラフにデータを追加
+            for (double x = 0; x <= 1; x += 0.01) // 0.01刻み
+            {
+                series.Points.Add(new DataPoint(x, IntegrateA(x,int.Parse(comboBox1.Text))));
+            }
+            model.Series.Add(series);
+            plotView.Model = model;
+
+        }
+
+        // 台形法による積分の関数
+        static double Integrate(double a, double b, int n, Func<double, double> f)
+        {
+            double h = (b - a) / n; // 各区間の幅
+            double sum = 0.5 * (f(a) + f(b)); // 両端の値を足す
+
+            for (int i = 1; i < n; i++)
+            {
+                double x = a + i * h;
+                sum += f(x);
+            }
+            return sum * h; // 積分結果を返す
+        }
+
+        // y = IntegrateA(x) の関数
+        static double IntegrateA(double b,int r)
+        {
+            int n = 1000; // 分割数
+            // 積分を計算
+            return Integrate(0, b, n, x => fc(x,r))/ Integrate(0, 1, n, x => fc(x, r)); // 例: f(x) = 
+           // return Math.Pow(b, r);
+        }
+
+
+        static double fc(double x,int r)
+        {
+            double rlt = 0;
+            rlt = Math.Pow(x,r) * Math.Pow(1 - x,r);            
+            return rlt;
+        }
+    }
+}
+
+
